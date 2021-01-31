edc294634d76d729c68a45151c3f240c317d9c73
Kinga Mamak <kingam280@gmail.com>
2021-01-31 21:12:49 +0100

    Resolve conflict after merge

--- src/assets/ts/main.ts
+++ src/assets/ts/main.ts
@@ -1,11 +1,5 @@
 import '../css/main.css';
-<<<<<<< HEAD
 import {TopPanel} from './TopPanel'
 export const isInternalLink = (link: string) => /^\/(?!\/)/.test(link);
 
 TopPanel()
-=======
-export const isInternalLink = (link: string) => /^\/(?!\/)/.test(link);
-
-
->>>>>>> master
