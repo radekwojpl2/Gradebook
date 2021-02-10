6f37a7e01f39bef12666bfc905bba0d28fa1bcfc
Kinga Mamak <kingam280@gmail.com>
2021-02-10 15:28:57 +0100

    Create function createAnnouncementsSections

---
+++ src/assets/ts/announcementsStudent/createAnnouncementsSections.ts
@@ -0,0 +1,20 @@
+import {appendChildrenToElement, createElementWithClasses, createElementWithInnerText} from './GlobalFunctions'
+
+export const createAnnouncementsSections = () => {
+    const textContent = `
+            <button class="show-all">Show All</button>
+            <button class="show-exams">Exams</button>
+            <button class="show-general">General</button>
+            <select name="sort" id="sort-input">
+                <option value="newest">From the newest</option>
+                <option value="oldest">From the oldest</option> 
+            </select>
+            `
+    const announcementsButtonsSection = createElementWithClasses('div', 'announcements-buttons')
+    const announcementsContainer = createElementWithClasses('div', 'announcements-container')
+    announcementsButtonsSection.innerHTML = textContent
+    const announcementsSection = document.querySelector('.announcements-section')!
+    announcementsSection.appendChild(announcementsButtonsSection)
+    announcementsSection.appendChild(announcementsContainer)
+    const ann = new AnnouncementsList()
+}
