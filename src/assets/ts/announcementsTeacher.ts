42dc439e5fa7eaaec86e8a225067134a8d5fd108
mateugazi <mateugazi182@outlook.com>
2021-02-10 16:38:10 +0100

    Created basic form layout

--- src/assets/ts/announcementsTeacher.ts
+++ src/assets/ts/announcementsTeacher.ts
@@ -1,1 +1,4 @@
 console.log('siemano kolano')
+import createFunction from './announcementsTeacher/teacherForm'
+const teacherFormDiv = document.querySelector('.teacherForm')
+teacherFormDiv?.appendChild(createFunction())
