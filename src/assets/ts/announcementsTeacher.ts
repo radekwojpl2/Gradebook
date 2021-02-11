b4d6eecac65d9de3bb1cb029cabc7b8fa4895433
mateugazi <mateugazi182@outlook.com>
2021-02-12 00:52:07 +0100

    Completed the submit functionallity without input validation

--- src/assets/ts/announcementsTeacher.ts
+++ src/assets/ts/announcementsTeacher.ts
@@ -3,16 +3,14 @@ import submitAnnouncementForm from './announcementsTeacher/submitAnnouncementFor
 import instance from '../../axios'
 const teacherFormDiv = document.querySelector('.teacherFormContainer')
 const form = createAnnouncementForm()
-console.log(form)
 teacherFormDiv?.appendChild(form)
-console.log(teacherFormDiv)
-form?.addEventListener('submit',submitAnnouncementForm)
-const user = {
-    yo: 'yo yo'
-}
-
-function firebasePostAnnouncements() {
-    instance.post('announcements.json', user)
-        .then(res => console.log(res))
-        .catch(err => console.log(err))
-}
+form?.addEventListener('submit',function(event) {
+    event.preventDefault()
+    const data = submitAnnouncementForm()
+    console.log(data)
+    /*
+    instance.post('announcements.json', data)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+    */
+})
--- src/assets/ts/announcementsTeacher/submitAnnouncementForm.ts
+++ src/assets/ts/announcementsTeacher/submitAnnouncementForm.ts
@@ -1,14 +1,25 @@
-export default function submitAnnouncementForm(event: { preventDefault: () => void } ) {
-    event.preventDefault()
+export default function submitAnnouncementForm() {
 
-    const type = (<HTMLInputElement>document.querySelector('.teacherForm__select__option')).value
-   /* if (type?.nodeValue === "important" || type === "normal" || type === "exam") {
-
-    }*/
+    const type = (<HTMLInputElement>document.querySelector('.teacherForm__select')).value
     const name = (<HTMLInputElement>document.querySelector('.teacherForm__inputName')).value
     const title = (<HTMLInputElement>document.querySelector('.teacherForm__inputTitle')).value
     const message = (<HTMLInputElement>document.querySelector('.teacherForm__message')).value
-    const date = (<HTMLInputElement>document.querySelector('.teacherForm__inputDate')).value
-
-    console.log(type, name, title, message, date)
+    if (type === 'exam') {
+        const date = (<HTMLInputElement>document.querySelector('.teacherForm__inputDate')).value
+        return {
+            type,
+            name,
+            title,
+            message,
+            date
+        }
+    } else {
+        return {
+            type,
+            name,
+            title,
+            message
+        }
+    }
+   
 }
