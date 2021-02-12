8d964328bc56d3f61bfcbb6d98bedc080cd1e68e
neacatsu <wiktoria.wierzbicka166@gmail.com>
2021-02-12 15:22:02 +0100

    showing student list and grades

--- src/assets/ts/gradesStudentsPanel.ts
+++ src/assets/ts/gradesStudentsPanel.ts
@@ -1,5 +1,71 @@
 import '../css/gradesStudentsPanel.css';
-import axios from '../../axios.ts';
+import axios from '../../axios';
+import { resolve } from 'path';
+import { unlink } from 'fs';
+import { emitKeypressEvents } from 'readline';
+import { UsersInterface, GradesInterface } from "./models/gradesStudentsPanel"
+
+let usersData : Array<UsersInterface> = [];
+let gradesData : Array<GradesInterface> = [];
+const button : HTMLElement | null = document.querySelector('.students > button');
+
+button?.addEventListener('click', () => {
+    const secret = (document.querySelector('.students > input') as HTMLTextAreaElement).value.trim();
+    const user_id : number | boolean = getId(secret);
+    console.log(user_id);
+
+    console.log(gradesData)
+    
+    outputGrades(user_id);
+});
 
 axios.get('/users.json')
-.then( response => {console.log(response.data)})
+.then( (response) => { 
+    usersData = response.data;
+    outputStudents(response.data);
+})
+.catch( (error) => { console.error(error) });
+
+axios.get('/grades.json')
+.then( (response) => { 
+    gradesData = response.data;
+})
+.catch( (error) => { console.error(error) });
+
+function getId(secret : string) : number | boolean {
+    let id : number | null = null;
+    usersData.forEach((element) => {
+        if(element.secret == secret)  {
+            id = element.user_id;
+        }
+    });
+    if(id !== null) return id;
+    return false;
+}
+
+function outputStudents(users : Object[]) : void {
+    var ulStudentList : HTMLElement | any = document.querySelector("#studentList");
+    var outputStudentList : string = ""
+   
+    users.forEach((element : Object | any)=> {
+        outputStudentList += `<li>${element.firstName}</li>`;
+    });
+    ulStudentList.innerHTML = outputStudentList;
+}
+
+function outputGrades(id : number | boolean) : void {
+    var ulStudentList : HTMLElement | any = document.querySelector("#studentGrades");
+    var outputList : string = ""
+
+    gradesData.forEach((element)=> {                
+        if(element.user_id  === id) {
+            outputList += `<li>${element.title} ${element.grade}</li>`;
+            console.log(element.user_id  === id);
+        }
+    });
+    if(outputList === "") {
+        outputList = "Podaj poprawny sekret";
+    }
+
+    ulStudentList.innerHTML = outputList;
+}
---
+++ src/assets/ts/models/gradesStudentsPanel.d.ts
@@ -0,0 +1,16 @@
+export interface UsersInterface {
+    firstName: string,
+    gender: string,
+    legitimationID: string, 
+    secondName: string, 
+    secret: string, 
+    user_id: number
+}
+
+export interface GradesInterface {
+    date: string,
+    grade: number,
+    subject: string, 
+    title: string, 
+    user_id: number
+}
