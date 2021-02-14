ed3dd446cfe3b9cbf6b28ed905f0e416570d2529
mateugazi <mateugazi182@outlook.com>
2021-02-14 16:00:01 +0100

    Fixed error with webpack

--- src/assets/ts/announcementsTeacher.ts
+++ src/assets/ts/announcementsTeacher.ts
@@ -8,10 +8,10 @@ teacherFormDiv?.appendChild(form)
 form?.addEventListener('submit',function(event) {
     event.preventDefault()
     const data = submitAnnouncementForm()
-    console.log(data)
-    /*
+    //console.log(data)
+    
     instance.post('announcements.json', data)
         .then(res => console.log(res))
         .catch(err => console.log(err))
-    */
+    
 })
